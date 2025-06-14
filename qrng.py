09772dc15eebd02a3639547e76b1a8fd0d3b2494
BharathB777 <165371260+BharathB777@users.noreply.github.com>
2025-06-14 18:00:03 +0530

    Initial QRNG web app with neon UI & quantum backend

--- qrng.py
+++ qrng.py
@@ -1,16 +1,16 @@
 from qiskit import QuantumCircuit
 from qiskit_aer import AerSimulator
-from qiskit.compiler import transpile, assemble
+from qiskit.transpiler import PassManager
+from qiskit.compiler import transpile
 
-def generate_quantum_bits(num_bits=1):
-    qc = QuantumCircuit(num_bits, num_bits)
-    qc.h(range(num_bits))  # Hadamard gate to create superposition
-    qc.measure(range(num_bits), range(num_bits))  # Measure qubits
+def generate_quantum_bit(n=1):
+    qc = QuantumCircuit(n, n)
+    qc.h(range(n))             # Apply Hadamard gates
+    qc.measure(range(n), range(n))  # Measure the qubits
 
     simulator = AerSimulator()
     compiled_circuit = transpile(qc, simulator)
-    qobj = assemble(compiled_circuit, shots=1)
-    result = simulator.run(qobj).result()
-
-    counts = result.get_counts(qc)
-    return list(counts.keys())[0]  # Return binary string (e.g., '01101001')
+    result = simulator.run(compiled_circuit, shots=1).result()
+    counts = result.get_counts()
+    bit_string = list(counts.keys())[0]
+    return bit_string
